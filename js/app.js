ccd109e15ffe72b608c39a91994a992f16ddb0f1
Sunny Master <master.codeworks@gmail.com>
2024-07-18 18:00:55 -0400

    add select no. of players option

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 console.log('this is linked')
 
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const totalPlayersEl = document.getElementById('players-select')
+console.log(totalPlayersEl)
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
 
 
 
