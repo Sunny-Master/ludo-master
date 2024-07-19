9f326c3722e565862e3d14e35c58486db6084a4c
Sunny Master <master.codeworks@gmail.com>
2024-07-19 09:01:01 -0400

    add button for dice

--- js/app.js
+++ js/app.js
@@ -5,19 +5,29 @@ console.log('this is linked')
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let numOfPlayers, numberOfPieces, diceValue, turn, selectedPiece, winner, outOfBounds
+let activePieceGreen, activePieceYellow, activePieceBlue, activePieceRed
 
 
 /*------------------------ Cached Element References ------------------------*/
-const totalPlayersEl = document.getElementById('players-select')
-console.log(totalPlayersEl)
+const yellowSection = document.querySelector('#yellowGateway')
+const blueSection = document.querySelector('#blueGateway')
+const redSection = document.querySelector('#redGateway')
+const greenSection = document.querySelector('#greenGateway')
 
+const redPiecesDepot = document.querySelector('#redBig')
+const greenPiecesDepot = document.querySelector('#greenBig')
+const yellowPiecesDepot = document.querySelector('#yellowBig')
+const bluePiecesDepot = document.querySelector('#blueBig')
 
+console.log(yellowPiecesDepot)
 /*-------------------------------- Functions --------------------------------*/
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+
 
 
 
