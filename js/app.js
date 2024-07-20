514ae72f801b1db1cfa1feb8c26fbb3c3c91e22d
Sunny Master <master.codeworks@gmail.com>
2024-07-20 02:01:40 -0400

    add first move condition in handleDiceRoll function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('this is linked')
 
 /*-------------------------------- Constants --------------------------------*/
-const piecesObject = {
+const pieceObject = {
     green: '💚',
     yellow: '💛',
     blue: '💙',
@@ -34,13 +34,15 @@ const pathWay = {
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let numOfPlayers, numOfPieces, diceValue, turn, selectedPiece, winner, outOfBounds, pieceHome
+let numOfPlayers, numOfPieces, diceValue, turn, selectedPiece, winner, outOfBounds, pieceHome, playArea
 let diceFaceWipe
 let board = []
 
 let homeDepots = []
 
 const activePieces = {}
+
+const piecePosition = {}
 
 /*------------------------ Cached Element References ------------------------*/
 const pathSquareEls = document.querySelectorAll('.path')
@@ -59,10 +61,10 @@ const greenSectionEl = document.querySelector('#greenGateway')
 
 const messageEl = document.getElementById('message')
 
-const redPiecesDepotEl = document.querySelector('#redBig')
-const greenPiecesDepotEl = document.querySelector('#greenBig')
-const yellowPiecesDepotEl = document.querySelector('#yellowBig')
-const bluePiecesDepotEl = document.querySelector('#blueBig')
+const redDepotEl = document.querySelector('#redBig')
+const greenDepotEl = document.querySelector('#greenBig')
+const yellowDepotEl = document.querySelector('#yellowBig')
+const blueDepotEl = document.querySelector('#blueBig')
 
 const diceEl = document.getElementById('dice')
 
@@ -84,11 +86,15 @@ function init() {
     numOfPieces = 4
     diceValue = 0
     turn = 'green'
-    selectedPiece = ''
-    activePieces.green = 4
-    activePieces.blue = 4
-    activePieces.red = 4
-    activePieces.yellow = 4
+    playArea = pathWay[turn]
+    activePieces.green = 0
+    activePieces.blue = 0
+    activePieces.red = 0
+    activePieces.yellow = 0
+    piecePosition.green = [88, 89, 90, 91]
+    piecePosition.yellow = [76, 77, 78, 79] 
+    piecePosition.blue = [80, 81, 82, 83]
+    piecePosition.red = [84, 85, 86, 87]
     winner = false
     pieceHome = false
     outOfBounds = false
@@ -101,62 +107,111 @@ init()
 function render() {
     updateBoard()
     updateMessage()
-    showDiceValue()
 }
 
 //to update the board with game state
 function updateBoard() {
     board.forEach((cell, idx) => {
         //console.log(pathSquareEls[idx])
-        pathEls[idx].textContent = piecesObject[cell]
+        pathEls[idx].textContent = pieceObject[cell]
     })
 }
 
 // to update the message element text to display the game state
 function updateMessage() {
-    if(winner === false && pieceHome === false) {
-        messageEl.textContent = `${piecesObject[turn]}'s turn. Please roll the dice and select piece to move`
-
+    if (!winner && !pieceHome && activePieces[turn] === 0) {
+        messageEl.textContent = `${pieceObject[turn]}'s turn. Please roll the dice to get a 6 to make your first move`
+    } else if(winner === false && pieceHome === false) {
+        messageEl.textContent = `${pieceObject[turn]}'s turn. Please roll the dice and select piece to move`
     } else if (winner === false && pieceHome) {
-        messageEl.textContent = `${piecesObject[turn]}'s piece reached home!! ${activePieces[turn]} more to Win`
+        messageEl.textContent = `${pieceObject[turn]}'s piece reached home!! ${activePieces[turn]} more piece(s) to Win`
     } else {
-        messageEl.textContent = `${piecesObject[turn]} won!! Congratulations!!`
+        messageEl.textContent = `${pieceObject[turn]} won!! Congratulations!!`
     }
 }
 
-// to display the value of rolled dice
-function showDiceValue() {
-    if (diceValue === 0) {
-        return
-    } else {
-        diceEl.textContent = ""
-        diceEl.classList.add(`d${diceValue}`)
-    }
-}
-
-function handleDice() {
+// to handle dice roll and randomly assign diceValue
+function handleDiceRoll() {
+    console.log('clicked')
     if (winner) {
         return
     }
-    if (diceValue !== 0) {
+    if (diceValue) {
         diceEl.classList.remove(diceFaceWipe)
     }
-    diceFaceWipe = `d${diceValue}`
     diceValue = Math.floor(Math.random() * 6) + 1
+    diceFaceWipe = `d${diceValue}`
+    //console.log(diceEl)
     showDiceValue()
+
+    // if there are no pieces on the player's pathway and dice value is 6, 
+    // activate the eventlistener for the player's depot
+    if (activePieces[turn] === 0 && diceValue !== 6) {
+        return
+    } else if (activePieces[turn] < 4 && diceValue === 6) {
+        greenDepotEl.addEventListener('click', handleDepot)
+    }
+    // pathEls.forEach(pathEl => {
+    //     if (piecePosition[turn].includes(pathEl.id)) {
+    //         pathEl.addEventListener('click', handleClick)
+    //     }
+    // })
+}
+    
+
+
+// to display the value of rolled dice
+function showDiceValue() {
+        diceEl.textContent = ""
+        diceEl.classList.add(`d${diceValue}`)
 }
 
-// to handle the piece selection by the player
-// function handleClick(event) {
-//     const squareIndex = parseInt(event.target.id)
+// to make the first move 
+function handleDepot(event) {
+    const pieceIndex = event.target.id
+    console.log(pieceIndex)
+    board[pieceIndex] = ''
+    const startPos = playArea[0]
+    console.log(startPos)
+    board[startPos] = turn
+    render()
+}
 
-// }
 
 
+
+//to handle the piece selection by the player
+function handleClick(event) {
+    outOfBounds = false //to switch-back outOfBounds to false after intial outOfBounds message
+    const squareIndex = parseInt(event.target.id)
+    movePiece(squareIndex)
+    render()
+}
+
+// to move the piece from current position to new position on the board
+function movePiece(index) {
+    const currentPos = playArea.indexOf(index)
+    const newPos = currentPos + diceValue
+    checkBounds(newPos, playArea)
+    console.log(outOfBounds)
+    if(!outOfBounds) {
+        board[index] = ''
+        board[playArea[newPos]] = turn
+        diceEl.textContent = '🎲'
+        diceEl.classList.remove(diceFaceWipe)
+    }
+}
+
+// to check if the new position is outOfBounds for the player
+function checkBounds(newPos, playArea) {
+    if(playArea[newPos] === undefined) {
+        outOfBounds = true
+    }
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
-diceEl.addEventListener('click', handleDice)
-//boardEl.addEventListener('click', handleClick)
+diceEl.addEventListener('click', handleDiceRoll)
+
 
 
 
